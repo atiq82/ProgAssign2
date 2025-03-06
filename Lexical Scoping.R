e1e430a6a34a38ff06f65b408857c060c72086c8
atiq82 <61285871+atiq82@users.noreply.github.com>
2025-03-06 11:24:32 -0600

    Update Lexical Scoping.R

--- Lexical Scoping.R
+++ Lexical Scoping.R
@@ -1,3 +1,16 @@
+## The file defines two functions: makeCacheMatrix and cacheSolve
+## 
+## makeCacheMatrix defines a list for caching the 
+##  matrix as well as its cached inverse and the functions
+##  used to maintain the cached matrices.
+## 
+## cacheSolve utilizes the list created by makeCacheMatrix
+##  to invert the cached matrix.  
+##  If the inverted matrix is already cached, then the cached
+##  matrix is returned.  If not, then the cached original, 
+##  non-inverted, matrix is inverted, cached and the inverted
+##  matrix returned.
+
 #MakeCacheMatrix#
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
